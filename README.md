c5a45ab1357ac2c619a541f03610b0ff0248dc2f
Miron Gofer <miron.gofer@sap.com>
2017-01-04 22:51:44 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# miovim
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'miovim'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures miovim'
+long_description 'Installs/Configures miovim'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/miovim/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/miovim' if respond_to?(:source_url)
