30d126725192a9cb4b661105a508f8c04aaf1d15
Miron Gofer <miron.gofer@sap.com>
2017-01-17 11:35:59 +0200

    some houskeeping

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
 name 'miovim'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'mio'
+maintainer_email 'miron@tikalk.com'
 license 'all_rights'
-description 'Installs/Configures miovim'
-long_description 'Installs/Configures miovim'
+description 'Installs/Configures vim and vim plugins using Vundle'
+long_description 'Installs/Configures vim and vim plugins'
 version '0.1.0'
 
+suports 'ubuntu', 'centos'
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
 # issues_url 'https://github.com/<insert_org_here>/miovim/issues' if respond_to?(:issues_url)
