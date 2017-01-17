04d3fc78411c00fcf04ba0ed671d6dbc564e8db5
Miron Gofer <miron.gofer@sap.com>
2017-01-17 11:50:41 +0200

    both contexts need to test the template

--- spec/unit/recipes/vim_spec.rb
+++ spec/unit/recipes/vim_spec.rb
@@ -8,18 +8,19 @@ require 'spec_helper'
 
 describe 'miovim::vim' do
     context 'When on ubuntu 16.04 platform' do
-    let(:chef_run) do
+        let(:chef_run) do
             runner = ChefSpec::SoloRunner.new(
                 platform: 'ubuntu',
                 version: '16.04') do |node|
                 node.normal['miovim']['home'] = '/home/mio'
             end
-      runner.converge(described_recipe)
-    end
+        runner.converge(described_recipe)
+        end
 
-    it 'installed successfully' do
-      expect(chef_run).to install_package('vim')
-    end
+        it 'installed successfully' do
+            expect(chef_run).to install_package('vim')
+            expect(chef_run).to create_template('/home/mio/.vimrc')
+        end
   end
     context 'When on centos platform' do
         let(:chef_run) do
