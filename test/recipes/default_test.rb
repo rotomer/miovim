66a343b1bdc45176d44fac81f2135b85a92fd95f
Miron Gofer <miron.gofer@sap.com>
2017-05-10 15:40:56 +0300

    some fix to default_test

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -4,3 +4,22 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
+if os.debian? 
+  describe package('vim') do
+    it { should be_installed }
+  end
+end
+if os.redhat?
+  describe package('vim-enhanced') do
+      it { should be_installed }
+  end
+end
+
+describe file("/home/vagrant/.vimrc") do
+    it { should exist }
+#    its('content') { should match(%r{config/vundle.vim}) }
+end
+#describe file("/home/vagrant/.vim/config/vundle.vim") do
+#    it { should exist }
+#    its('content') { should match(%r{tpope/vim-fugitive}) }
+#end
